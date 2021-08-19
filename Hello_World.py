e437362822d52195f9d2122ee0d29bddef38799a
Jexxy <jexxytheitguy@gmail.com>
2021-08-19 13:58:57 -0400

    Made cooler hello world

---
+++ Hello_World.py
@@ -0,0 +1,11 @@
+# Terrible naming convention, do not follow
+going_to_cyber_school = input("Type something cool ")
+
+if going_to_cyber_school == "cool":
+    print("hello world")
+elif going_to_cyber_school == "not cool":
+    print("You probably use facebook.")
+else:
+    print("You can literally read the code, even a5 year old could figure out how to make this do something")
+
+# Did not wanna do something complicated like jon and amos, but I did not want a regular hello world
