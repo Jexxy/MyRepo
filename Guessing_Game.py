19476c17eaa763f158d634f4760148690c2e16dc
Jexxy <jexxytheitguy@gmail.com>
2021-08-24 18:31:30 -0400

    new file

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -11,13 +11,22 @@
 
 # After the game, ask the user if they would like to play again (10 points)
 
-import random
+import random as rand
+import sys
+from os import system
+from sys import platform
+
+if platform == "win32":
+    cls = lambda: system('cls')
+elif platform == "linux":
+    cls = lambda: system('clear')
 
 
 # This function will continue to loop forever until given usable input or told to exit.
 def get_guess():
     while True:  # Use a while loop here so it will continue the program unless otherwise told, and will not crash.
         try:  # Get user's guess
+            cls()
             guess_num = int(input('''Please enter a number between 1-100 to make your guess: '''))
         except ValueError:  # Catches the ValueError that happens when people don't put in a number
             while True:  # Starts a loop here so they HAVE to type Y or N, any other input simply wont work.
@@ -41,19 +50,34 @@ def get_guess():
 
 
 def main():
-    game_iteration = 0
+    random_number = rand.randrange(1, 100)
+    game_iteration = 1
     while True:
         ready_to_start = input('''Welcome to the guessing game!
-                         A Random number between 1-100 has already been generated!
-                         When ready, please type "Y" to start: ''')
+        A Random number between 1-100 has already been generated!
+        When ready, please type "Y" to start or type "N" to exit: ''')
         if ready_to_start.lower() != "y":
-            continue
+            if ready_to_start.lower() == "n":
+                exit(0)
+            else:
+                print("Please type Y OR N!")
+                continue
         else:
             break
     while True:
-
-
-    print(get_guess())
-
+        print(random_number)
+        user_guess = get_guess()
+        if game_iteration == 5:
+            print("You have guessed five times! You lost!")
+            break
+        elif user_guess == random_number:
+            print("Congrats! You have guessed correctly!")
+            break
+        elif user_guess < random_number:
+            print("Your guess was lower than the random number.")
+        elif user_guess > random_number:
+            print("Your guess was higher than the random number.")
+        game_iteration += 1
+    play_again = input("Would you like to play again? Y/N: ")
 
 main()
